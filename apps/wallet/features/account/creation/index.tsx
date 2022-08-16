8c0ebb874f258f28be68fe18bc7ef52c05a5131b
ohager <oliver@devbutze.com>
2022-08-16 10:44:00 -0300

    feat: define your pin step

--- apps/wallet/features/account/creation/index.tsx
+++ apps/wallet/features/account/creation/index.tsx
@@ -1,11 +1,15 @@
-import { Body } from "@/app/components/layout/body";
-import { Container } from "@/app/components/layout/container";
-import { Button } from "react-daisyui";
 import { Stepper } from "@/app/components/stepper";
 import { FC, useEffect, useMemo, useState } from "react";
 import { BottomNavigationItem } from "@/app/components/navigation/bottomNavigation";
 import { RiArrowLeftCircleLine, RiArrowRightCircleLine } from "react-icons/ri";
 import { voidFn } from "@/app/voidFn";
+import { useRouter } from "next/router";
+import {
+  StepOne,
+  StepTwo,
+  StepThree,
+  StepFour,
+} from "@/features/account/creation/steps";
 
 export interface OnStepChangeArgs {
   steps: number;
@@ -18,15 +22,20 @@ interface Props {
 }
 
 export const AccountCreation: FC<Props> = ({ onStepChange }) => {
+  const router = useRouter();
   const StepCount = 4;
   const [currentStep, setCurrentStep] = useState<number>(0);
 
-  const nextStep = () => {
-    setCurrentStep(Math.min(currentStep + 1, StepCount - 1));
+  const nextStep = async () => {
+    const newStep = Math.min(currentStep + 1, StepCount - 1);
+    setCurrentStep(newStep);
+    router.push(`#step${newStep}`, `#step${newStep}`, { shallow: true });
   };
 
   const previousStep = () => {
-    setCurrentStep(Math.max(0, currentStep - 1));
+    const newStep = Math.max(0, currentStep - 1);
+    setCurrentStep(newStep);
+    router.push(`#step${newStep}`, `#step${newStep}`, { shallow: true });
   };
 
   useEffect(() => {
@@ -72,6 +81,25 @@ export const AccountCreation: FC<Props> = ({ onStepChange }) => {
     <>
       <div className="mt-4">
         <Stepper currentStep={currentStep} steps={StepCount}></Stepper>
+        <div className="carousel w-full">
+          {/*<div id="step0" className="carousel-item relative w-full">*/}
+          {/*  <div className="mt-4 prose">*/}
+          {/*    <h1>Account Creation</h1>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+          <div id="step0" className="carousel-item relative w-full">
+            <StepOne />
+          </div>
+          <div id="step1" className="carousel-item relative w-full">
+            <StepTwo />
+          </div>
+          <div id="step2" className="carousel-item relative w-full">
+            <StepThree />
+          </div>
+          <div id="step3" className="carousel-item relative w-full">
+            <StepFour />
+          </div>
+        </div>
       </div>
     </>
   );
--- apps/wallet/features/account/creation/steps/step1.tsx
+++ apps/wallet/features/account/creation/steps/step1.tsx
@@ -1,7 +1,34 @@
+import { useTranslation } from "next-i18next";
+import { Input } from "react-daisyui";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+
 export const StepOne = () => {
+  const { t } = useTranslation();
+
+  const [pinValue, setPinValue] = useState("");
+
+  const handlePinChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log("PIN", e.target.value);
+  };
+
   return (
-    <div>
-      <h1>Step One</h1>
+    <div className="flex flex-col justify-center content-center text-center h-[90vh] relative prose max-w-none">
+      <section>
+        <h2>{t("define_pin")}</h2>
+      </section>
+      <section className="relative top-[15%]">
+        <Input
+          className="text-center"
+          type={"password"}
+          size="lg"
+          maxLength={7}
+          onChange={handlePinChange}
+          placeholder="ENTER YOUR SECRET"
+        />
+      </section>
+      <section className="w-[75%] m-auto text-justify border border-base-content/50 p-4 rounded relative top-8">
+        <small>{t("define_pin_hint")}</small>
+      </section>
     </div>
   );
 };
