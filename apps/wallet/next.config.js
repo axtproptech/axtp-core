792b0dfadfc4f462db32b5df573aecef43d9faf3
ohager <oliver@devbutze.com>
2022-11-17 09:46:07 -0300

    chore: removed db dist

--- apps/wallet/next.config.js
+++ apps/wallet/next.config.js
@@ -1,18 +1,16 @@
 /** @type {import("next").NextConfig} */
 
 const { i18n } = require("./next-i18next.config");
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest\.json$/],
+});
 const withTM = require("next-transpile-modules")(["react-daisyui"]);
 module.exports = withPWA(
   withTM({
-    debug: process.env.NODE_ENV === "development",
-    pwa: {
-      dest: "public",
-      runtimeCaching,
-      disable: process.env.NODE_ENV === "development",
-      buildExcludes: [/middleware-manifest\.json$/],
-    },
     reactStrictMode: true,
     i18n,
   })
--- packages/db/dist/index.mjs
+++
@@ -1,6 +0,0 @@
-// index.ts
-import { PrismaClient } from "@prisma/client";
-var prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-export { prisma };
-//# sourceMappingURL=index.mjs.map
