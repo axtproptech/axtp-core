be719114a95b635aaabc72dac30bf57ae597401a
ohager <oliver@devbutze.com>
2023-02-17 10:32:49 -0300

    wallet/fix: Junte-se and icon for CTA button

--- apps/wallet/app/components/buttons/joinClubButton.tsx
+++ apps/wallet/app/components/buttons/joinClubButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from "react-daisyui";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+import { RiGroupLine } from "react-icons/ri";
 
 export const JoinClubButton = () => {
   const { t } = useTranslation();
@@ -13,6 +14,7 @@ export const JoinClubButton = () => {
   return (
     <div className="animate-wiggle">
       <Button color="primary" size="lg" onClick={handleOnJoinClick}>
+        <RiGroupLine className="mr-2" size="28" />
         {t("join_club")}
       </Button>
     </div>
--- apps/wallet/app/components/buttons/showAccountButton.tsx
+++ apps/wallet/app/components/buttons/showAccountButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from "react-daisyui";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+import { RiWallet3Line } from "react-icons/ri";
 
 export const ShowAccountButton = () => {
   const { t } = useTranslation();
@@ -13,6 +14,7 @@ export const ShowAccountButton = () => {
   return (
     <div className="animate-wiggle">
       <Button color="accent" size="lg" onClick={handleOnJoinClick}>
+        <RiWallet3Line className="mr-2" size="28" />
         {t("my_portfolio")}
       </Button>
     </div>
