71044df392e994d94a764d1d5fb702b27333ee4b
ohager <oliver@devbutze.com>
2023-02-22 17:46:33 -0300

    backoffice/feat: Refund cards applied (UI)

--- apps/backoffice/app/services/ledgerService/poolContractDataView.ts
+++ apps/backoffice/app/services/ledgerService/poolContractDataView.ts
@@ -2,6 +2,7 @@ import { Contract, ContractDataView } from "@signumjs/contracts";
 import { toStableCoinAmount } from "@/app/tokenQuantity";
 import { ApprovalStatus } from "@/types/approvalStatus";
 
+// FIXME: add indices for ApprovedRefund and pendingRefund
 enum PoolContractDataIndex {
   PoolName = 5,
   PoolRate,
@@ -92,6 +93,13 @@ export class PoolContractDataView {
     return parseFloat(toStableCoinAmount(qnt));
   }
 
+  getRefundableStableCoins(): number {
+    return 0;
+    // FIXME: implement
+    // const qnt = this.view.getVariableAsDecimal(PoolContractDataIndex.PaidAXTC);
+    // return parseFloat(toStableCoinAmount(qnt));
+  }
+
   getDistributionApprovalStatus(): ApprovalStatus {
     const approvedAccounts = [];
     const approved1 = this.getApprovedAccount(
@@ -119,6 +127,40 @@ export class PoolContractDataView {
     return {
       approvedAccounts,
       quantity: "0", // this is the current contracts AXTC balance
+    };
+  }
+
+  getRefundApprovalStatus(): ApprovalStatus {
+    // FIXME: implement
+    const approvedAccounts = [];
+    const approved1 = this.getApprovedAccount(
+      PoolContractDataIndex.ApprovalApprovedDistribution1,
+      PoolContractDataIndex.ApprovalAccount1
+    );
+    const approved2 = this.getApprovedAccount(
+      PoolContractDataIndex.ApprovalApprovedDistribution2,
+      PoolContractDataIndex.ApprovalAccount2
+    );
+    const approved3 = this.getApprovedAccount(
+      PoolContractDataIndex.ApprovalApprovedDistribution3,
+      PoolContractDataIndex.ApprovalAccount3
+    );
+    const approved4 = this.getApprovedAccount(
+      PoolContractDataIndex.ApprovalApprovedDistribution4,
+      PoolContractDataIndex.ApprovalAccount4
+    );
+
+    approved1 && approvedAccounts.push(approved1);
+    approved2 && approvedAccounts.push(approved2);
+    approved3 && approvedAccounts.push(approved3);
+    approved4 && approvedAccounts.push(approved4);
+
+    // FIXME: add
+    // const quantity = this.view.getVariableAsDecimal(PoolContractDataIndex.PendingRefund);
+
+    return {
+      approvedAccounts,
+      quantity: "0",
     };
   }
 }
---
+++ apps/backoffice/features/pools/view/manage/refundApprovalCard/index.tsx
@@ -0,0 +1,99 @@
+import { ActionCard } from "@/app/components/cards";
+import { IconUserCheck } from "@tabler/icons";
+import { Box } from "@mui/material";
+import { ApprovalStepper } from "@/app/components/approvalStepper";
+import { FC, useMemo } from "react";
+import { ApprovalStatus } from "@/types/approvalStatus";
+import { useLedgerAction } from "@/app/hooks/useLedgerAction";
+import { SucceededTransactionSection } from "@/app/components/sections/succeededTransactionSection";
+import { asRSAddress } from "@/app/asRSAddress";
+import { useAccount } from "@/app/hooks/useAccount";
+import { BasicTokenInfo } from "@/types/basicTokenInfo";
+import { Undo } from "@mui/icons-material";
+import { usePoolContract } from "@/app/hooks/usePoolContract";
+import { useMasterContract } from "@/app/hooks/useMasterContract";
+import { Number } from "@/app/components/number";
+
+const getApprovalState = (status: ApprovalStatus, token: BasicTokenInfo) => {
+  const hasPayoutBalance = parseInt(status.quantity, 10) > 0;
+  return [
+    {
+      label: hasPayoutBalance ? (
+        <div>
+          Available for Refund&nbsp;
+          <Number value={status.quantity} decimals={2} suffix={token.name} />
+        </div>
+      ) : (
+        `No ${token.name} balance`
+      ),
+      icon: <Undo />,
+      completed: hasPayoutBalance,
+    },
+    {
+      label: status.approvedAccounts[0]
+        ? `Approved by ${asRSAddress(status.approvedAccounts[0])}`
+        : "1st Approval",
+      icon: <IconUserCheck />,
+      completed: !!status.approvedAccounts[0],
+    },
+    {
+      label: status.approvedAccounts[1]
+        ? `Approved by ${asRSAddress(status.approvedAccounts[1])}`
+        : "2nd Approval",
+      icon: <IconUserCheck />,
+      completed: !!status.approvedAccounts[1],
+    },
+    {
+      label: status.approvedAccounts[2]
+        ? `Approved by ${asRSAddress(status.approvedAccounts[2])}`
+        : "3rd Approval",
+      icon: <IconUserCheck />,
+      completed: !!status.approvedAccounts[2],
+    },
+  ];
+};
+
+interface Props {
+  poolId: string;
+}
+
+export const RefundApprovalCard: FC<Props> = ({ poolId }) => {
+  const { accountId } = useAccount();
+  const { token: masterToken } = useMasterContract();
+  const { approvalStatusRefund } = usePoolContract(poolId);
+  const { execute, isExecuting, transactionId } = useLedgerAction();
+
+  const approvalState = useMemo(() => {
+    return getApprovalState(approvalStatusRefund, masterToken);
+  }, [approvalStatusRefund, masterToken]);
+
+  const canApprove = useMemo(() => {
+    if (!accountId) return false;
+
+    const hasPayoutBalance = parseFloat(approvalStatusRefund.quantity) > 0;
+    const hasApprovedAlready =
+      approvalStatusRefund.approvedAccounts.includes(accountId);
+    return hasPayoutBalance && !hasApprovedAlready;
+  }, [approvalStatusRefund, accountId]);
+
+  const handleOnApproveAction = () => {
+    execute((service) => service.poolContract.with(poolId).approveAXTCRefund());
+  };
+
+  return (
+    <ActionCard
+      title={`Approve ${masterToken.name} Refund`}
+      description="Here you can see if there's a pending refund request and its current approval state"
+      actionIcon={<IconUserCheck />}
+      actionLabel="Approve Refund"
+      onClick={handleOnApproveAction}
+      isLoading={isExecuting}
+      disabled={!canApprove}
+    >
+      <Box sx={{ width: "100%", p: 0.5 }}>
+        <ApprovalStepper state={approvalState} />
+      </Box>
+      <SucceededTransactionSection transactionId={transactionId} />
+    </ActionCard>
+  );
+};
