dc9076beee5af2e5d7ed344462c5d367743b3b35
ohager <oliver@devbutze.com>
2023-04-21 18:47:03 -0300

    chore: type fixes

--- apps/wallet/app/formatDate.ts
+++ apps/wallet/app/formatDate.ts
@@ -8,7 +8,7 @@ import { ptBR, enUS } from "date-fns/locale";
 
 export interface FormatNumberArgs {
   date: Date | string;
-  locale?: "en-US" | "pt-BR";
+  locale?: string;
 }
 
 const localization = {
@@ -26,10 +26,13 @@ export function formatDate({ date, locale = "pt-BR" }: FormatNumberArgs) {
   const d = typeof date === "string" ? new Date(date) : date;
   const delta = differenceInHours(now, d);
   if (delta < 4) {
+    // @ts-ignore
     return formatDistance(d, now, { locale: localization[locale] || enUS });
   } else if (delta < 48) {
+    // @ts-ignore
     return formatRelative(d, now, { locale: localization[locale] || enUS });
   } else {
+    // @ts-ignore
     return format(d, formats[locale] || formats["en-US"]);
   }
 }
