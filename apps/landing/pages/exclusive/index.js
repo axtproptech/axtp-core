760afce04a2c89fcceb77ab7788e9dbc0b0e9527
ipr0310 <35440957+ipr0310@users.noreply.github.com>
2023-05-04 10:37:24 -0400

    Delete exclusive-temp folder

--- apps/landing/pages/exclusive/index.js
+++ apps/landing/pages/exclusive/index.js
@@ -2,79 +2,63 @@ import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 import { theme } from "common/theme/cryptoModern";
 import ResetCSS from "common/assets/css/style";
-import Sticky from "react-stickynode";
-import Navbar from "containers/CryptoModern/Navbar";
-// import Banner from "containers/CryptoModern/Banner";
-// import CountDown from "containers/CryptoModern/CountDown";
-// import Features from "containers/CryptoModern/FeatureSection";
-// import WorkHistory from "containers/CryptoModern/WorkHistory";
-// import Investment from "containers/CryptoModern/Investment";
-// import FundRaising from "containers/CryptoModern/FundRaising";
-// import Privacypolicy from "containers/CryptoModern/Privacy";
-// import WalletSection from "containers/CryptoModern/WalletSection";
-// import MapSection from "containers/CryptoModern/MapSection";
-// import FaqSection from "containers/CryptoModern/FaqSection";
-// import Newsletter from "containers/CryptoModern/Newsletter";
+import Navbar from "containers/Exclusive/components/Navbar";
 import Footer from "containers/CryptoModern/Footer";
+import ExclusiveAreaPage from "containers/Exclusive/LandingPage/index";
 import GlobalStyle, {
   CryptoWrapper,
   ContentWrapper,
 } from "containers/CryptoModern/cryptoModern.style";
 
-const CryptoModern = () => {
+const ExclusiveLandingPage = () => {
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <Head>
-          <title>AXT PropTech Company S/A - Exlusive Area</title>
-          <meta
-            name="Description"
-            content="AXT PropTech Company S/A - Exclusive Area"
-          />
-          <meta name="theme-color" content="#2563FF" />
-          <meta
-            name="keywords"
-            content="blockchain, proptech, web3, signum, smart contracts"
-          />
+      <Head>
+        <title>AXT PropTech Company S/A - Exlusive Area</title>
+        <meta
+          name="Description"
+          content="AXT PropTech Company S/A - Exclusive Area"
+        />
+        <meta name="theme-color" content="#2563FF" />
+        <meta
+          name="keywords"
+          content="blockchain, proptech, web3, signum, smart contracts"
+        />
 
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-        </Head>
-        {/* end of head */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
 
-        <ResetCSS />
-        <GlobalStyle />
-        {/* end of global and reset style */}
+      <ResetCSS />
+      <GlobalStyle />
 
-        {/* start app classic landing */}
-        <CryptoWrapper>
-          <Sticky top={0} innerZ={9999} activeClass="sticky-active">
-            <Navbar />
-          </Sticky>
-          <ContentWrapper>
-            <h2>Welcome to this exclusive area</h2>
-          </ContentWrapper>
-          <Footer />
-        </CryptoWrapper>
-        {/* end of app classic landing */}
-      </>
+      <Navbar />
+
+      <CryptoWrapper>
+        <ContentWrapper>
+          <ExclusiveAreaPage />
+        </ContentWrapper>
+
+        <Footer />
+      </CryptoWrapper>
     </ThemeProvider>
   );
 };
-export default CryptoModern;
+
+export default ExclusiveLandingPage;
