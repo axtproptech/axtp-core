bfbb09d6606434fa648f60d3c1269442ec49177b
ohager <oliver@devbutze.com>
2023-05-08 14:54:27 -0300

    wallet/feat: fixes #123 - update pre-registered user

--- apps/wallet/bff/handler/customer/registerCustomer.ts
+++ apps/wallet/bff/handler/customer/registerCustomer.ts
@@ -27,6 +27,7 @@ export const registerCustomer: RouteHandlerFunction = async (req, res) => {
       },
     });
 
+    // check if is registered already
     const existingCustomer = await prisma.customer.findUnique({
       where: {
         cpfCnpj: answers.cpf,
@@ -43,6 +44,12 @@ export const registerCustomer: RouteHandlerFunction = async (req, res) => {
       return;
     }
 
+    const preregisteredCustomer = await prisma.customer.findFirst({
+      where: {
+        email1: answers.email,
+      },
+    });
+
     const documents = answers.residentProofUrls.map((url) => ({
       type: "ProofOfAddress",
       url,
@@ -54,41 +61,83 @@ export const registerCustomer: RouteHandlerFunction = async (req, res) => {
       }))
     );
 
-    const newCustomer = await prisma.customer.create({
-      data: {
-        firstName: answers.fullName.first,
-        lastName: answers.fullName.last,
-        firstNameMother: answers.mothersName.first,
-        lastNameMother: answers.mothersName.last,
-        email1: answers.email,
-        cpfCnpj: answers.cpf,
-        dateOfBirth: answers.birthDate,
-        placeOfBirth: answers.birthPlace,
-        verificationLevel: "Pending",
-        nationality: "",
-        phone1: answers.phone,
-        profession: answers.occupation,
-        documents: {
-          createMany: {
-            // @ts-ignore
-            data: documents,
+    let newCustomer;
+    if (preregisteredCustomer) {
+      newCustomer = await prisma.customer.update({
+        where: {
+          cuid: preregisteredCustomer.cuid,
+        },
+        data: {
+          firstName: answers.fullName.first,
+          lastName: answers.fullName.last,
+          firstNameMother: answers.mothersName.first,
+          lastNameMother: answers.mothersName.last,
+          email1: answers.email,
+          cpfCnpj: answers.cpf,
+          dateOfBirth: answers.birthDate,
+          placeOfBirth: answers.birthPlace,
+          verificationLevel: "Pending",
+          nationality: "",
+          phone1: answers.phone,
+          profession: answers.occupation,
+          documents: {
+            createMany: {
+              // @ts-ignore
+              data: documents,
+            },
+          },
+          addresses: {
+            create: {
+              state: answers.address.state,
+              city: answers.address.city,
+              type: "Residential",
+              country: answers.address.country,
+              line1: answers.address.addr_line1,
+              line2: answers.address.addr_line2 || "",
+              line3: "",
+              line4: "",
+              postCodeZip: answers.address.postal,
+            },
           },
         },
-        addresses: {
-          create: {
-            state: answers.address.state,
-            city: answers.address.city,
-            type: "Residential",
-            country: answers.address.country,
-            line1: answers.address.addr_line1,
-            line2: answers.address.addr_line2 || "",
-            line3: "",
-            line4: "",
-            postCodeZip: answers.address.postal,
+      });
+    } else {
+      newCustomer = await prisma.customer.create({
+        data: {
+          firstName: answers.fullName.first,
+          lastName: answers.fullName.last,
+          firstNameMother: answers.mothersName.first,
+          lastNameMother: answers.mothersName.last,
+          email1: answers.email,
+          cpfCnpj: answers.cpf,
+          dateOfBirth: answers.birthDate,
+          placeOfBirth: answers.birthPlace,
+          verificationLevel: "Pending",
+          nationality: "",
+          phone1: answers.phone,
+          profession: answers.occupation,
+          documents: {
+            createMany: {
+              // @ts-ignore
+              data: documents,
+            },
+          },
+          addresses: {
+            create: {
+              state: answers.address.state,
+              city: answers.address.city,
+              type: "Residential",
+              country: answers.address.country,
+              line1: answers.address.addr_line1,
+              line2: answers.address.addr_line2 || "",
+              line3: "",
+              line4: "",
+              postCodeZip: answers.address.postal,
+            },
           },
         },
-      },
-    });
+      });
+    }
 
     bffLoggingService.info({
       msg: "New customer registered",
