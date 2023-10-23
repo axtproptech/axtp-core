2c40aa40f76819899efac6c2bf81c5a5aecc30ed
ohager <oliver@devbutze.com>
2023-10-23 18:37:58 -0300

    wip: create contact in crm lso

---
+++ apps/wallet/bff/crmClient.ts
@@ -0,0 +1,58 @@
+// @ts-ignore
+import * as Brevo from "@getbrevo/brevo";
+
+interface CreateContactArgs {
+  email: string;
+  cuid: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  birthDate: Date;
+  isTokenHolder?: boolean;
+}
+
+interface UpdateContactArgs {
+  email: string;
+  cuid: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  birthDate: Date;
+  isTokenHolder: boolean;
+  isVerified: boolean;
+}
+
+const ContactListId = {
+  KycCompleted: 7,
+};
+
+// This is a candidate for @axtp/core, as we will need to update customers/users from the backoffice also...
+export class CrmClient {
+  private contactsApi = new Brevo.ContactsApi();
+
+  constructor(apiKey: string) {
+    Brevo.ApiClient.instance.authentications["api-key"].apiKey = apiKey;
+  }
+
+  public async createNewContact(args: CreateContactArgs) {
+    const contact = new Brevo.CreateContact();
+    contact.email = args.email;
+    contact.ext_id = args.cuid;
+    contact.listIds = [ContactListId.KycCompleted];
+    contact.attributes = {
+      FIRSTNAME: args.firstName,
+      LASTNAME: args.lastName,
+      PHONE: args.phone,
+      BIRTHDATE: args.birthDate,
+      IS_TOKENHOLDER: args.isTokenHolder ?? false,
+      IS_VERIFIED: false,
+    };
+
+    return this.contactsApi.createContact(contact);
+  }
+
+  public async updateContact(args: UpdateContactArgs) {
+    throw new Error("Not implemented yet");
+    // const contact = new Brevo.UpdateContact()
+  }
+}
