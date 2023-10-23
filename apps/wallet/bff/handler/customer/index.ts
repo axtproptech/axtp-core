9afa6108dda383cc2749a145fd87f43bddba7344
ohager <oliver@devbutze.com>
2023-10-23 16:12:09 -0300

    chore: cleaned up index.ts file

--- apps/wallet/bff/handler/customer/index.ts
+++ apps/wallet/bff/handler/customer/index.ts
@@ -1,7 +1,5 @@
 export * from "./registerCustomerFromJotform";
-export * from "./registerCustomer/registerCustomer";
+export * from "./registerCustomer";
 export * from "./getCustomer";
 export * from "./searchCustomer";
 export * from "./addPublicKey";
-export { createBlockchainAccountData } from "@/bff/handler/customer/registerCustomer/createBlockchainAccountData";
-export { sendSuccessfulRegistrationMails } from "@/bff/handler/customer/registerCustomer/sendSuccessfulRegistrationMails";
