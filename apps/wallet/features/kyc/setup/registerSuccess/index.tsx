4d7fd7dcf90cf3ba99058847b21a7dff3b327ea0
ohager <oliver@devbutze.com>
2023-10-24 13:22:42 -0300

    feat: success member card

--- apps/wallet/features/kyc/setup/registerSuccess/index.tsx
+++ apps/wallet/features/kyc/setup/registerSuccess/index.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from "next-i18next";
 import { useAppDispatch } from "@/states/hooks";
 import { accountActions } from "@/app/states/accountState";
 import { CustomerSafeData } from "@/types/customerSafeData";
+import { useAppContext } from "@/app/hooks/useAppContext";
+import { Address } from "@signumjs/core";
 
 interface Props {
   customer: CustomerSafeData;
@@ -10,8 +12,9 @@ interface Props {
 
 export const RegisterSuccess: FC<Props> = ({ customer }) => {
   const { t } = useTranslation();
-  const { firstName } = customer;
+  const { firstName, publicKey } = customer;
   const dispatch = useAppDispatch();
+  const { Ledger } = useAppContext();
 
   useEffect(() => {
     if (!customer) return;
@@ -22,42 +25,47 @@ export const RegisterSuccess: FC<Props> = ({ customer }) => {
   return (
     <div className="flex flex-col text-center h-[80vh] justify-center items-center relative prose w-full mx-auto">
       <section>
-        <div className="overflow-hidden flex flex-col justify-center items-center gap-2 relative w-[300px] h-[150px] rounded-xl mx-auto">
+        <div className="overflow-hidden gap-2 relative py-8 rounded-xl flex flex-col items-center">
           <div
             className="w-full h-full absolute top-0 left-0 m-0 p-0 z-0"
             style={{
               background:
-                " linear-gradient(rgba(0,0,0,0.60), rgba(0,0,0,0.60)),url(/assets/img/cardGradientBackground.jpg)",
+                " linear-gradient(rgba(0,0,0,0.40), rgba(0,0,0,0.60)),url(/assets/img/cardGradientBackground.jpg)",
               backgroundSize: "cover",
               backgroundPosition: "bottom",
             }}
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              className="w-full h-full absolute top-0 left-0 m-0 p-0 z-10"
+              className="w-full h-full absolute top-0 left-0 m-0 p-0 z-10 glance-effect"
               src="/assets/img/card-grid.webp"
               alt="Card grid"
             />
           </div>
 
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/assets/img/axt-text-logo-2.svg"
-            className="w-1/2 z-10 m-0"
-            alt="AXT Logo"
-          />
-
-          <span className="text-zinc-300 text-center font-bold z-10">
-            {t("axt_club_member")}
-          </span>
+          <div className="flex flex-row items-start justify-start gap-x-4 px-4 drop-shadow min-w-[380px] w-full h-16">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/assets/img/axt-logo-only.svg"
+              className="w-[64px] top-[-32px] z-10 relative"
+              alt="AXT Logo"
+            />
+            <span className="text-zinc-300 text-xl font-bold z-10">
+              {t("axt_club_member")}
+            </span>
+          </div>
+          <div className="flex flex-col items-start justify-center z-10 text-zinc-200 font-mono">
+            <span>{firstName}</span>
+            <span>
+              {Address.fromPublicKey(publicKey).getReedSolomonAddress(false)}
+            </span>
+          </div>
         </div>
       </section>
 
       <section className="px-4">
-        <h3>{t("register_welcome", { firstName })}</h3>
-        <p className="text-white text-justify font-bold">
-          {t("register_description")}
-        </p>
+        <h2>{t("register_welcome", { firstName })}</h2>
+        <p className="text-justify">{t("register_description")}</p>
       </section>
     </div>
   );
