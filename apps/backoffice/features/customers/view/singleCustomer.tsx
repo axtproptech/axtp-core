4e9bd50f935b0c9e89398c9e8a283572e6ecd9d0
Caique Gonçalves <caique1994_1@hotmail.com>
2023-10-27 16:29:14 -0300

    edit user data on backoffice

--- apps/backoffice/features/customers/view/singleCustomer.tsx
+++ apps/backoffice/features/customers/view/singleCustomer.tsx
@@ -4,6 +4,8 @@ import {
   CircularProgress,
   Divider,
   Grid,
+  InputAdornment,
+  TextField,
   Tooltip,
   Typography,
 } from "@mui/material";
@@ -12,7 +14,7 @@ import {
   ErrorOutline as ErrorIcon,
 } from "@mui/icons-material";
 import { Config } from "@/app/config";
-import { FC, useMemo } from "react";
+import { FC, useMemo, useState } from "react";
 import { customerService } from "@/app/services/customerService/customerService";
 import useSWR, { useSWRConfig } from "swr";
 import { MainCard } from "@/app/components/cards";
@@ -31,6 +33,18 @@ import { OpenExplorerButton } from "@/app/components/buttons/openExplorerButton"
 import { useRouter } from "next/router";
 import { toDateStr } from "@/app/toDateStr";
 import { auth0Service } from "@/app/services/auth0Service";
+import { Controller, useForm } from "react-hook-form";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import EditIcon from "@mui/icons-material/Edit";
+import {
+  DesktopDatePicker as DatePicker,
+  LocalizationProvider,
+} from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { format, isValid } from "date-fns";
+import AddressEditor from "./components/addressEditor";
+
 const gridSpacing = Config.Layout.GridSpacing;
 export const SingleCustomer = () => {
   const { query } = useRouter();
@@ -130,6 +144,86 @@ export const SingleCustomer = () => {
   const loading = !customer && !error;
 
   // TODO: handle error
+  interface EditableNameProps {
+    initialFirstName: string;
+    initialLastName: string;
+    nationalityIcon: string;
+    onSave: (name: { firstName: string; lastName: string }) => void;
+  }
+
+  const EditableName = ({
+    initialFirstName,
+    initialLastName,
+    nationalityIcon,
+    onSave,
+  }: EditableNameProps) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [firstName, setFirstName] = useState(initialFirstName);
+    const [lastName, setLastName] = useState(initialLastName);
+
+    const handleSave = () => {
+      setIsEditing(false);
+      onSave({ firstName, lastName });
+    };
+
+    const handleCancelClick = () => {
+      setIsEditing(false);
+    };
+
+    return (
+      <Grid container spacing={1} direction="row" alignItems="center">
+        {isEditing ? (
+          <>
+            <Grid item>
+              <TextField
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                label="First Name"
+                size="small"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                label="Last Name"
+                size="small"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item>
+              <Tooltip title="Save">
+                <IconButton size="small" onClick={handleSave}>
+                  <CheckIcon fontSize="small" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="cancel">
+                <IconButton onClick={handleCancelClick}>
+                  <CloseIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid item>
+              <Typography variant="h4">
+                {`${firstName} ${lastName} ${nationalityIcon}`}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Tooltip title="Edit Name">
+                <IconButton size="small" onClick={() => setIsEditing(true)}>
+                  <EditIcon fontSize="small" />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </>
+        )}
+      </Grid>
+    );
+  };
 
   const title = useMemo(() => {
     if (loading || !customer) return "Loading...";
@@ -139,11 +233,17 @@ export const SingleCustomer = () => {
     return (
       <Grid container spacing={gridSpacing} direction="row" alignItems="center">
         <Grid item>
-          <Typography variant="h4">
-            {`${customer?.firstName} ${customer?.lastName} ${
-              customer.isInBrazil ? "🇧🇷" : "🌐"
-            }`}
-          </Typography>
+          <EditableName
+            initialFirstName={customer?.firstName}
+            initialLastName={customer?.lastName}
+            nationalityIcon={customer.isInBrazil ? "🇧🇷" : "🌐"}
+            onSave={async (newNames) => {
+              await customerService.with(cuid).updateCustomer({
+                firstName: newNames.firstName,
+                lastName: newNames.lastName,
+              });
+            }}
+          />
         </Grid>
         <Grid item>
           <VerificationChip level={verificationLevel} />
@@ -200,8 +300,131 @@ interface DetailsProps {
   customer: CustomerFullResponse;
 }
 
+interface EditableFieldProps {
+  label: string;
+  initialValue: string;
+  name: string;
+  type?: string;
+  onSubmit: (name: string, value: string) => void;
+}
+
+const EditableField = ({
+  label,
+  initialValue,
+  name,
+  onSubmit,
+  type,
+}: EditableFieldProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState<String | Date | null>(initialValue);
+  const { register, handleSubmit, control } = useForm();
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
+  const onFieldSubmit = (data: any) => {
+    onSubmit(name, data);
+    setIsEditing(false);
+  };
+
+  return (
+    <Grid container spacing={2} alignItems="center">
+      {isEditing ? (
+        <form
+          style={{ padding: "16px 0" }}
+          onSubmit={handleSubmit(onFieldSubmit)}
+        >
+          {type === "date" ? (
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Controller
+                name={name}
+                control={control}
+                defaultValue={initialValue}
+                render={({ field }) => (
+                  <DatePicker
+                    {...field}
+                    value={field.value}
+                    onChange={(date) => {
+                      field.onChange(date);
+                      isValid(date) && setValue(format(date, `dd/MM/yyyy`));
+                    }}
+                    inputFormat="dd/MM/yyyy"
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label={label}
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton type="submit">
+                                <CheckIcon />
+                              </IconButton>
+                              <IconButton onClick={handleCancelClick}>
+                                <CloseIcon />
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }}
+                        fullWidth
+                      />
+                    )}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+          ) : (
+            <TextField
+              {...register(name)}
+              defaultValue={value}
+              label={label}
+              fullWidth
+              onChange={(e) => setValue(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton type="submit">
+                      <CheckIcon />
+                    </IconButton>
+                    <IconButton onClick={handleCancelClick}>
+                      <CloseIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+        </form>
+      ) : (
+        <>
+          <Grid item xs>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Typography>
+                {label}:{" "}
+                {value instanceof Date ? format(value, `dd/MM/yyyy`) : value}
+              </Typography>
+              <IconButton
+                style={{ marginLeft: "1rem" }}
+                onClick={handleEditClick}
+              >
+                <EditIcon />
+              </IconButton>
+            </div>
+          </Grid>
+        </>
+      )}
+    </Grid>
+  );
+};
+
 const CustomerDetails: FC<DetailsProps> = ({ customer }) => {
+  const { query } = useRouter();
   const address = customer.addresses.length && customer.addresses[0];
+  const cuid = query.cuid as string;
   const account =
     customer.blockchainAccounts.length && customer.blockchainAccounts[0];
   const documents = customer.documents;
@@ -210,43 +433,64 @@ const CustomerDetails: FC<DetailsProps> = ({ customer }) => {
     return cpf.isValid(customer.cpfCnpj);
   }, [customer.cpfCnpj]);
 
+  const handleFieldValueChange = async (_: string, updatedData: any) => {
+    await customerService.with(cuid).updateCustomer(updatedData);
+  };
+
   return (
     <Grid container spacing={gridSpacing} direction="column">
       <Grid item xs={12} md={6}>
         <Grid container spacing={gridSpacing}>
           <Grid item xs={12} md={6} lg={4}>
-            <LabeledTextField label="CPF">
-              <Grid container columnSpacing={1} direction="row">
-                <Grid item>{cpf.format(customer.cpfCnpj)}</Grid>
-                <Grid item>
-                  {cpfValid ? (
-                    <Tooltip title="Valid Format">
-                      <CheckIcon color="success" />
-                    </Tooltip>
-                  ) : (
-                    <Tooltip title="Format Incorrect">
-                      <ErrorIcon color="error" />
-                    </Tooltip>
-                  )}
-                </Grid>
-                <Grid item>
-                  <ExternalLink
-                    href={
-                      "https://servicos.receita.fazenda.gov.br/servicos/cpf/consultasituacao/consultapublica.asp"
-                    }
-                  >
-                    Validate CPF
-                  </ExternalLink>
-                </Grid>
+            <EditableField
+              label="CPF"
+              initialValue={cpf.format(customer.cpfCnpj)}
+              name="cpfCnpj"
+              onSubmit={handleFieldValueChange}
+            />
+            <Grid
+              container
+              columnSpacing={1}
+              direction="row"
+              alignItems="center"
+            >
+              <Grid item>
+                {cpfValid ? (
+                  <Tooltip title="Valid Format">
+                    <CheckIcon color="success" />
+                  </Tooltip>
+                ) : (
+                  <Tooltip title="Format Incorrect">
+                    <ErrorIcon color="error" />
+                  </Tooltip>
+                )}
               </Grid>
-            </LabeledTextField>
-            <LabeledTextField label="Phone Number" text={customer.phone1} />
+              <Grid item>
+                <ExternalLink href="https://servicos.receita.fazenda.gov.br/servicos/cpf/consultasituacao/consultapublica.asp">
+                  Validate CPF
+                </ExternalLink>
+              </Grid>
+            </Grid>
+            <EditableField
+              label="Phone Number"
+              initialValue={customer.phone1}
+              name="phone1"
+              onSubmit={handleFieldValueChange}
+            />
             <LabeledTextField label="Email" text={customer.email1} />
-            <LabeledTextField
-              label="Date and Place of Birth"
-              text={`${toDateStr(new Date(customer.dateOfBirth))}, ${
-                customer.placeOfBirth
-              }`}
+
+            <EditableField
+              label="Date of Birth"
+              initialValue={toDateStr(new Date(customer.dateOfBirth))}
+              name="dateOfBirth"
+              onSubmit={handleFieldValueChange}
+              type="date"
+            />
+            <EditableField
+              label="Place of Birth"
+              initialValue={customer.placeOfBirth}
+              name="placeOfBirth"
+              onSubmit={handleFieldValueChange}
             />
             <LabeledTextField
               label="Nationality"
@@ -273,24 +517,22 @@ const CustomerDetails: FC<DetailsProps> = ({ customer }) => {
             {address ? (
               <>
                 <LabeledTextField label="Street" text={address.line1} />
+
                 <LabeledTextField label="Complement" text={address.line2} />
-                <LabeledTextField
-                  label="Annotation"
-                  text={`${address.line3} ${address.line4}`}
-                />
-                <LabeledTextField
-                  label="ZipCode"
-                  text={`${address.postCodeZip}`}
-                />
-                <LabeledTextField
-                  label="City, State"
-                  text={`${address.city}, ${address.state}`}
-                />
-                <LabeledTextField label="Country" text={`${address.country}`} />
+
+                <LabeledTextField label="Annotation" text={address.line3} />
+
+                <LabeledTextField label="ZipCode" text={address.postCodeZip} />
+
+                <LabeledTextField label="City" text={address.city} />
+                <LabeledTextField label="State" text={address.state} />
+
+                <LabeledTextField label="Country" text={address.country} />
               </>
             ) : (
               <Typography variant="h4">No address provided</Typography>
             )}
+            <AddressEditor address={address} />
           </Grid>
           <Grid item xs={12} md={6} lg={4}>
             {account ? (
