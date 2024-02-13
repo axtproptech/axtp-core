25aadba4e990454830a85a29516a0ce9c49550d1
ohager <oliver@devbutze.com>
2024-02-13 11:46:54 -0300

    feat: added withdrawal record

---
+++ packages/core/withdrawalRecord/withdrawalRecordHelper.ts
@@ -0,0 +1,60 @@
+import { Transaction } from "@signumjs/core";
+import { DescriptorData, DescriptorDataBuilder } from "@signumjs/standards";
+import { decryptMessage } from "@signumjs/crypto";
+import { WithdrawalRecord, PaymentType } from "./withdrawalRecord";
+
+export function createSRC44WithdrawalRecord(
+  args: WithdrawalRecord
+): DescriptorData {
+  return DescriptorDataBuilder.create()
+    .setAccount(args.accountId)
+    .setCustomField("x-cuid", args.customerId)
+    .setCustomField("x-tid", args.tokenId)
+    .setCustomField("x-tqnt", args.tokenQuantity)
+    .setCustomField("x-tnm", args.tokenName)
+    .setCustomField("x-pt", args.paymentType)
+    .setCustomField("x-pa", args.paymentAmount)
+    .setCustomField("x-usd", args.paymentUsd)
+    .setCustomField("x-cur", args.paymentCurrency)
+    .setCustomField("x-ptx", args.paymentTransactionId)
+    .build();
+}
+
+export function getWithdrawalRecordFromSRC44(
+  descriptor: DescriptorData
+): WithdrawalRecord {
+  const cx = (k: string) => descriptor.getCustomField(k) as string;
+
+  return {
+    accountId: descriptor.account,
+    customerId: cx("x-cuid"),
+    tokenId: cx("x-tid"),
+    tokenQuantity: cx("x-tqnt"),
+    tokenName: cx("x-tnm"),
+    paymentType: cx("x-pt") as PaymentType,
+    paymentAmount: cx("x-pa"),
+    paymentCurrency: cx("x-cur"),
+    paymentUsd: cx("x-usd"),
+    paymentTransactionId: cx("x-ptx"),
+  };
+}
+
+export function readSRC44WithdrawalFromTransaction(
+  tx: Transaction,
+  recipientPrivateKeyHex?: string
+) {
+  let message = tx.attachment.message || "";
+
+  if (tx.attachment.encryptedMessage && recipientPrivateKeyHex) {
+    message = decryptMessage(
+      tx.attachment.encryptedMessage,
+      tx.senderPublicKey,
+      recipientPrivateKeyHex
+    );
+  }
+
+  if (!message) {
+    throw new Error("Could not read message");
+  }
+  return DescriptorData.parse(message);
+}
